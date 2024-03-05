361ddd9e36f62a2cc219ba0f494a59e8598f497e
nicolassaraivaa <nicolas.saraivaa@gmail.com>
2024-03-05 16:34:25 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const buttonDraw = document.querySelector(".draw-button")
+const resultDraw = document.querySelector(".result-draw")
+const divResultDraw = document.querySelector(".div-result")
 
 function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min; /* */
 
-    alert(result)
+    if (isNaN(min) || isNaN(max)) {
+        alert("Por favor, insira valores numéricos válidos.");
+        return;
+    }
 
-}
+    if (min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo.");
+        return;
+    }
 
-buttonDraw.addEventListener("click", generateNumber)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min; 
+
+    resultDraw.innerHTML = `O número sorteado é = ${result}`
+
+    buttonDraw.style.marginTop = "45px"
+    divResultDraw.style.visibility = 'visible'
+
+ }
+
+ buttonDraw.addEventListener("click", generateNumber)
